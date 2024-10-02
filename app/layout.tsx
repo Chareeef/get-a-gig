7aec8fb7643cea6b8f6b3ad24f26d29ef572e21c
Chareeef <youssef.charif.h@gmail.com>
2024-10-02 12:46:26 +0000

    Initialize Next.js project

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Get a Gig!",
+  description: "Uncover that job made just for you!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
