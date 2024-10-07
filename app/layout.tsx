0b2ea7debfd635dabdae62d080d9d50aec189647
Chareeef <youssef.charif.h@gmail.com>
2024-10-07 11:54:57 +0000

    Reorganize the project's structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import type { Metadata } from "next";
-import "./globals.css";
+import "@/styles/globals.css";
 
 export const metadata: Metadata = {
   title: "Get a Gig!",
