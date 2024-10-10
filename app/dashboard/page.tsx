c8d2a9c739b69c2bc0824c672164bec405607790
Chareeef <youssef.charif.h@gmail.com>
2024-10-10 16:20:05 +0100

    Implement loading state

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,8 @@
 "use client";
-import { useSession, signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { satoshi } from "../fonts/fonts";
 
 const Dashboard = () => {
   const { data: session, status } = useSession();
@@ -14,22 +15,23 @@ const Dashboard = () => {
   }, [session, status, router]);
 
   if (status === "loading") {
-    return <div>Loading...</div>; // Loading state while session is being fetched
+    // Show a loading indicator
+    return (
+      <div className="flex-center grow bg-gray-100 dark:bg-slate-500">
+        <span
+          className={`${satoshi.className} animate-spin text-2xl font-black text-yellow-500 ease-in-out`}
+        >
+          @
+        </span>
+      </div>
+    );
   }
 
   return (
-    <div>
+    <main className="flex grow flex-col bg-gray-100 p-4 dark:bg-slate-500">
       <h1>Welcome, {session?.user?.name || session?.user?.email}</h1>
       {/* Dashboard content */}
-
-      {/* Logout Button */}
-      <button
-        onClick={() => signOut({ callbackUrl: "/auth/signin" })}
-        className="rounded-md border-2 border-amber-50 bg-slate-300 hover:bg-slate-400"
-      >
-        Log Out
-      </button>
-    </div>
+    </main>
   );
 };
 
